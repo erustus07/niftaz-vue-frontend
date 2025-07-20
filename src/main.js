751302146de861066eb6aac11e34da41821321ba
erustus07 <erustusndegwa08@gmail.com>
2025-07-20 12:57:41 +0300

    Update: home page and layout changes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import 'bootstrap/dist/css/bootstrap.min.css'
 
-// createApp(App).mount('#app')
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router'
-
-// createApp(App).use(router).mount('#app')
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
