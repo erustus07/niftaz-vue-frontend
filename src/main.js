8fa6bce36fb7bb769e78fb551d28134f98989359
erustus07 <erustusndegwa08@gmail.com>
2025-07-20 12:55:54 +0300

    third  Vue frontend commit for NIFTAZ

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 // createApp(App).mount('#app')
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// createApp(App).use(router).mount('#app')
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
